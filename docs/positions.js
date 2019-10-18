059656945574e2f0066a2d6e72ee6af9f974a0fa
Igor Kostyuchenok <igor@mbanq.com>
2019-10-18 11:13:25 +0200

    Minor changes

---
+++ docs/positions.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    brief: 'Currently Product Lead and Software Engineer',
+    company: 'Mbanq Cloud'
+  },
+  {
+    brief: 'First ever open banking API and Marketplace',
+    company: 'Fidor Bank'
+  }
+]
