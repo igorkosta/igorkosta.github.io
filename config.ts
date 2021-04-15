fd22c1b39ab4f00d6f083bbbebee42d44ccc5c5b
Igor Kostyuchenok <igor@mbanq.com>
2021-04-15 14:00:25 +0200

    Added some more information

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ export default {
   description: 'my personal page',
   about: `
     <p>
-      I'm a Product and Tech Lead living in Munich.
+      I'm a <span class="highlight">Product and Tech Lead</span> living in Munich.
     </p>
     <p>
       I have more than a decade of professional experience building and launching products for web, mobile and desktop.
@@ -12,10 +12,16 @@ export default {
       I was responsible for launching the first version of the most used banking software on the German market: <a href="https://apps.apple.com/de/app/starmoney-financemanagement/id1237596145" target="_blank">StarMoney</a> for the Mac OS platform.
     </p>
     <p>
-      I led the development of worldwide first Open Banking API at <a href="https://www.fidor.com/solutions/developer" target="_blank">Fidor Bank</a>. I managed two teams responsible for the Banking API and the Banking App Store.
+      I <span class="highlight">led the development of worldwide first Open Banking API</span> at <a href="https://www.fidor.com/solutions/developer" target="_blank">Fidor Bank</a>. I managed two teams responsible for the Banking API and the Banking App Store.
     </p>
     <p>
-      I currently lead the product and software development of the Cloud Banking Platform at <a href="https://mbanq.com/cloud" target="_blank">Mbanq</a>. We build and easy to customize Digital Banking Platform that can be rolled out in hours. We use serverless 90% of our time.
+      I currently <span class="highlight">lead the product and software development</span> of the Cloud Banking Platform at <a href="https://mbanq.com/cloud" target="_blank">Mbanq</a> where we build an easy to customize Digital Banking Platform that can be rolled out in hours.
+    </p>
+    <p>
+      I'm also a <span class="highlight">part of the Founding Team</span> at <a href="https://www.cometum.com" target="_blank">Cometum</a> where we are building the Investment Platform for the new generation.
+    </p>
+    <p>
+      <span class="highlight"><a href="https://www.serverless.com/" target="_blank">Serverless</a> is the stack of my choice</span> at the moment. Check out the <span class="highlight">Open Source</span> section for some of the goodies for the development of the serverless applications.
     </p>
     <p>
       I'm running <a href="https://shows.acast.com/salty-breeze-radio/episodes" target="_blank">Salty Breeze Radio</a> - a weekly music podcast and writing <a href="https://ryan-raiz.github.io/avanti/" target="_blank">Avanti</a> - a fictional book about real life.
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 import { JssProvider, SheetsRegistry } from 'react-jss'
 import Header from '../components/layouts/header'
+import '../styles/costinha.css'
 
 const App = ({ Component, pageProps }) => {
   const [themeType, setThemeType] = useState('light')
